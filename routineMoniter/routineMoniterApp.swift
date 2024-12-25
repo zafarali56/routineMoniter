e43a4922b972ab375d77b1c2020a84aac39b8bc0
zafar ali <zafaralyjamali722@gmail.com>
2024-12-25 20:20:12 +0500

    Initial Commit

---
+++ routineMoniter/routineMoniterApp.swift
@@ -0,0 +1,17 @@
+//
+//  routineMoniterApp.swift
+//  routineMoniter
+//
+//  Created by Zafar Ali on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct routineMoniterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
